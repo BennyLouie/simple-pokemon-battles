6e37c695823c2e3b62eef7faeab64bd0dcaafb62
BennyLouie <bennylouie1412@gmail.com>
2019-11-22 11:22:55 -0500

    created fetch for random opponent

--- src/containers/BattleContainer.js
+++ src/containers/BattleContainer.js
@@ -5,12 +5,27 @@ import Box from "@material-ui/core/Box"
 
 export default class BattleContainer extends React.Component {
 
+    state = {
+        opponent_pokemon: null
+    }
+
+    componentDidMount() {
+        const opponent_pokemon_roll = Math.floor(Math.random() * 151) + 1
+        fetch(`http://localhost:3000/pokemons/${opponent_pokemon_roll}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                opponent_pokemon: data
+            })
+        })
+    }
+
     render() {
         console.log(this.props)
         return (
             <Box>
                 <UserField />
-                <OpponentField />
+                <OpponentField pokemon={this.state.opponent_pokemon} />
             </Box>
         )
     }
--- src/containers/OpponentField.js
+++ src/containers/OpponentField.js
@@ -4,7 +4,8 @@ import BattlePokemonCard from './BattlePokemonCard'
 
 export default class OpponentField extends React.Component {
 
-  render() {
+    render() {
+      console.log(this.props)
       return (
         <Box>
           <BattlePokemonCard />
