3a0d34807b988bdbf67f65e39702d3db1bcd4bf1
BennyLouie <bennylouie1412@gmail.com>
2019-11-26 10:38:46 -0500

    made messages auto scroll to the latest

--- src/containers/MessagesContainer.js
+++ src/containers/MessagesContainer.js
@@ -4,10 +4,19 @@ import Message from '../components/Message'
 
 export default class MessagesContainer extends React.Component {
 
+    constructor() {
+        super()
+        this.scrollRefContainer = React.createRef()
+    }
+
+    componentDidUpdate() {
+    this.scrollRefContainer.current.scrollTop = 99999999999999999999
+    }
+
     render() {
-        // console.log(this.props.messages)
+        console.log(this.props.messages)
         return (
-            <div className='messages-container'>
+            <div className='messages-container' ref={this.scrollRefContainer}>
                 {this.props.messages.map((message, idx) => <Message key={idx} message={message} />)}
             </div>
         )
