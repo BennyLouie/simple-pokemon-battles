554038db0e28348f7a6b8b34fe6555253dd65e33
BennyLouie <bennylouie1412@gmail.com>
2019-12-02 15:29:24 -0500

    added functions to buttons to update state of stats

--- src/containers/StatsContainer.js
+++ src/containers/StatsContainer.js
@@ -2,17 +2,44 @@ import React from 'react'
 
 export default class StatsContainer extends React.Component {
 
+    state = {
+        hp: this.props.pokemon.hp,
+        atk: this.props.pokemon.atk,
+        def: this.props.pokemon.def,
+        spd: this.props.pokemon.spd,
+        stat_pts: this.props.pokemon.stat_pts
+    }
+
+    addToStat = (stat) => {
+        if (this.state.stat_pts > 0) {
+            this.setState({
+                [stat]: this.state[stat] + 1,
+                stat_pts: this.state.stat_pts - 1
+            })
+        }
+    }
+
+    subtractFromStat = (stat) => {
+        if ((this.state.stat_pts < this.props.pokemon.stat_pts) && (this.state[stat] > this.props.pokemon[stat])) {
+            this.setState({
+                [stat]: this.state[stat] - 1,
+                stat_pts: this.state.stat_pts + 1
+            })
+        }
+    }
+
     render() {
-        debugger
-        console.log(this.props.pokemon)
+        // debugger
+        // console.log(this.props.pokemon)
+        console.log(this.state)
         return (
             <div className='stats-container'>
-                <p>HP: {this.props.pokemon.hp} <button>+</button><button>-</button></p>
-                <p>ATK: {this.props.pokemon.atk} <button>+</button><button>-</button></p>
-                <p>DEF: {this.props.pokemon.def} <button>+</button><button>-</button></p>
-                <p>SPD: {this.props.pokemon.spd} <button>+</button><button>-</button></p>
+                <p>HP: {this.state.hp} <button onClick={() => this.addToStat('hp')}>+</button><button onClick={() => this.subtractFromStat('hp')}>-</button></p>
+                <p>ATK: {this.state.atk} <button onClick={() => this.addToStat('atk')}>+</button><button onClick={() => this.subtractFromStat('atk')}>-</button></p>
+                <p>DEF: {this.state.def} <button onClick={() => this.addToStat('def')}>+</button><button onClick={() => this.subtractFromStat('def')}>-</button></p>
+                <p>SPD: {this.state.spd} <button onClick={() => this.addToStat('spd')}>+</button><button onClick={() => this.subtractFromStat('spd')}>-</button></p>
                 <p>EXP: {this.props.pokemon.exp} / {this.props.pokemon.exp_max}</p>
-                <p>Available Stat Points: {this.props.pokemon.stat_pts}</p>
+                <p>Available Stat Points: {this.state.stat_pts}</p>
             </div>
         )
     }
