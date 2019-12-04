069315403ae47dc7d147c297eabad811d26b2590
BennyLouie <bennylouie1412@gmail.com>
2019-12-04 11:03:37 -0500

    CSS work...

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,12 +6,11 @@ export default class Pokemon extends React.Component {
     render() {
         // console.log(this.props)
         return (
-            <Card className='card'>
+            <Card>
                 <Image className='ui small centered' style={{'background': 'none'}} src={this.props.user ? this.props.pokemon.back_img : this.props.pokemon.front_img} alt={this.props.pokemon.name} />
-                <Card.Content style={{ 'background': 'black' }}>
+                <Card.Content style={{ 'background': 'black'}}>
                 <Card.Header style={{'color': 'white'}}>{this.props.pokemon.name}</Card.Header>
                 <Card.Meta style={{'color': 'white'}}>Lv. {this.props.pokemon.lv}</Card.Meta>
-
                 </Card.Content>
             </Card>
         )
--- src/containers/UserPokemon.js
+++ src/containers/UserPokemon.js
@@ -9,10 +9,10 @@ export default class UserPokemon extends React.Component {
         return (
             <div id={`${this.props.pokemon.name}-card`} className='pc-pokemon'>
                 <Pokemon pokemon={this.props.pokemon} />
-                <div className='links'>
-                    <NavLink to='/update-pokemon' className='pokemon-status btn' onClick={() => this.props.selectPokemon(this.props.pokemon)}>Status Update</NavLink>
-                    <button className='release btn' onClick={() => this.props.releasePokemon(this.props.user, this.props.pokemon)} >Release Pokemon</button>
-                    <NavLink to='/battle' className='battle btn' onClick={() => this.props.selectPokemon(this.props.pokemon)}>Battle</NavLink>
+                <div className='team links'>
+                    <NavLink to='/update-pokemon' className='pokemon-status team status btn' onClick={() => this.props.selectPokemon(this.props.pokemon)}>Status</NavLink>
+                    <button className='release team btn' onClick={() => this.props.releasePokemon(this.props.user, this.props.pokemon)} >Release</button>
+                    <NavLink to='/battle' className='battle team btn' onClick={() => this.props.selectPokemon(this.props.pokemon)}>Battle</NavLink>
                 </div>
             </div>
         )
